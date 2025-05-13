1368ca5133293dbd0dcb0479ee502d3177779d49
Lee Goddard <leegee@gmail.com>
2025-05-13 11:01:11 +0200

    Decruft

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,10 @@ import { NextRequest, NextResponse } from 'next/server'
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    // 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 }
 
 export function middleware(request: NextRequest) {
-    console.log('Middleware hit:', request.nextUrl.pathname)
-
     const isPreflight = request.method === 'OPTIONS';
 
     if (isPreflight) {
