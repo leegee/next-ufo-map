ec2355799ae567d142d9913ffdbdea5964fa1bb3
Lee Goddard <leegee@gmail.com>
2025-05-14 12:58:53 +0200

    Quote fields with form field values

--- src/app/lib/server/csv.ts
+++ src/app/lib/server/csv.ts
@@ -8,7 +8,8 @@ export function listToCsvLine(values: unknown[], stream: Writable): void {
 
 function formatForCsv(value: unknown): string {
     const str = String(value);
-    if (/[,"\n]/.test(str)) {
+    // Quote if line endings or a quote mark:
+    if (/[,"\n\r\f]/.test(str)) {
         return `"${str.replace(/"/g, '""')}"`;
     }
     return str;
