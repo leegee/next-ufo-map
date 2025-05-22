7fd74784ff68f820e93136ca2984d8082c200f22
Lee Goddard <leegee@gmail.com>
2025-05-22 17:37:59 +0200

    Decruft

--- src/app/components/LocaleButton.tsx
+++ src/app/components/LocaleButton.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import './LocaleButton.scss';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'react-intl-universal';
 
